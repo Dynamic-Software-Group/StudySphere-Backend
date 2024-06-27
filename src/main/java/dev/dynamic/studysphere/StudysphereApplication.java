15e8dac5a1115cc4c45e5bf8761d419e00cbd50a
Incbom <99501939+IncbomDev@users.noreply.github.com>
2024-06-27 17:52:35 -0400

    Initial push

---
+++ src/main/java/dev/dynamic/studysphere/StudysphereApplication.java
@@ -0,0 +1,13 @@
+package dev.dynamic.studysphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudysphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudysphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dynamic/studysphere/StudysphereApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dynamic.studysphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudysphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
